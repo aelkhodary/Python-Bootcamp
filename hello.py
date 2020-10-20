2ae4f7603f96990edae64f1eccc21bbe8462c1bc
aelkhodary <40409487+aelkhodary@users.noreply.github.com>
2020-10-20 04:40:30 +0400

    second commit

--- hello.py
+++ hello.py
@@ -1,1 +1,22 @@
 print("hello world")
+a=10
+a=a+a
+print(a)
+a='ali'
+print(type(a))
+print("Hello world"[8])
+x = "Hello World"
+print(x.upper())
+
+print('The {} {} {}'.format('fox','brown','quick'))
+print('The {2} {1} {0}'.format('fox','brown','quick'))
+print('The {0} {0} {0}'.format('fox','brown','quick'))
+print('The {q} {b} {f}'.format(f='fox',b='brown',q='quick'))
+#Float formatting follows "{value:width.precision f}"
+result=100/777
+print("The result was {r}".format(r=result))
+print("The result was {r:1.3f}".format(r=result))
+
+# f string new in bython 3.6
+name="Ali"
+print(f'Hello ,his name is {name}')
