42a8cb79bed4f131e99d3c8fe51dc1f77f3e6c11
aelkhodary <aelkhodary1987@gmail.com>
2020-12-14 02:32:32 +0400

    start image module

---
+++ section16/image.py
@@ -0,0 +1,19 @@
+'''
+* Work with images with Python
+* The Pillow library which is afork of the PIL (Python Imaging Library)
+* https://pillow.readthedocs.io/en/stable/
+>>   pip install pillow 'at your command line'
+
+'''
+
+from PIL import Image
+mac = Image.open('C:\\Users\\amelkhodary\\Downloads\\img.jfif')
+print(type(mac))
+'''
+<class 'PIL.JpegImagePlugin.JpegImageFile'>
+'''
+mac.show()
+print(mac.size)
+print(mac.filename)
+mac.crop((0,0,25,25))
+mac.show()
